70d7b15d3b222d595a1f216f92bdfbd50860b4a4
sander-hergarten <mail@sander-hergarten.de>
2023-05-08 20:21:17 +0200

    merged os_input.c from Versuch1

--- src/SPOS/os_input.c
+++ src/SPOS/os_input.c
@@ -2,7 +2,6 @@
 
 #include <avr/io.h>
 #include <stdint.h>
-
 /*! \file
 
 Everything that is necessary to get the input from the Buttons in a clean format.
@@ -18,27 +17,36 @@ Everything that is necessary to get the input from the Buttons in a clean format
  *           4 Buttons: 1,3,4 -pushed: 000001101
  *
  */
-uint8_t os_getInput(void) {
-    #warning IMPLEMENT STH. HERE
-}
 
+uint8_t os_getInput(void)
+{
+    uint8_t pinState_C1_C2 = (PINC & 0b00000011);
+    uint8_t pinState_C3_C4 = (PINC >> 4) & 0b00001100;
+    return (~(pinState_C1_C2 | pinState_C3_C4) & bitmask);
+}
 /*!
  *  Initializes DDR and PORT for input
  */
-void os_initInput() {
-    #warning IMPLEMENT STH. HERE
+void os_initInput()
+{
+    DDRC &= 0b00111100;  // set pins C0, C1, C6, C7 as inputs
+    PORTC |= 0b11000011; // enable pull-up resistors for pins C0, C1, C6, C7
 }
 
 /*!
  *  Endless loop as long as at least one button is pressed.
  */
-void os_waitForNoInput() {
-    #warning IMPLEMENT STH. HERE
+void os_waitForNoInput()
+{
+    while ((os_getInput() & 0b00001111))
+        ;
 }
 
 /*!
  *  Endless loop until at least one button is pressed.
  */
-void os_waitForInput() {
-    #warning IMPLEMENT STH. HERE
+void os_waitForInput()
+{
+    while (os_getInput() == 0)
+        ;
 }
